e5e748fec0da68ae84e7178baae6c68322a7d60f
bency <bency@pixnet.tw>
2019-11-09 01:25:05 +0800

    轉移匯入工作到 Freeway

--- app/Console/Commands/import.php
+++ app/Console/Commands/import.php
@@ -3,7 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use DB;
+use App\Services\Freeway;
 
 class import extends Command
 {
@@ -40,66 +40,31 @@ class import extends Command
     public function handle()
     {
         $this->path = $this->argument('csvfolder');
+        $this->freeway = new Freeway($this->path);
         $end_year = date('Y');
         $start_year = $this->ask("要從哪一年開始匯入?", 2015);
         $start_month = $this->ask("要從哪一月開始匯入?", 1);
         $start_day = $this->ask("要從哪一天開始匯入?", 1);
-        $import_data = [];
         for ($year = $start_year; $year <= 2019; $year++) {
             for ($month = 1; $month <= 12; $month++) {
                 if ($year == $start_year and $month < $start_month) {
                     continue;
-                }
-                if ($month < 10) {
-                    $month = "0" . $month;
                 }
                 $max_day = date('t', strtotime($year . '/' . $month));
                 for ($day = 1; $day <= $max_day; $day++) {
                     if ($year == $start_year and $month == $start_month and $day < $start_day) {
                         continue;
                     }
-                    if ($day < 10) {
-                        $day = "0" . $day;
-                    }
-                    $date = $year . $month . $day;
                     for ($hour = 0; $hour <= 23; $hour++) {
-                        if ($hour < 10) {
-                            $hour = "0" . $hour;
-                        }
-                        $rows = $this->getRow($date, $hour);
                         try {
-                            $bar = $this->output->createProgressBar($this->getLineCount($date, $hour));
                             $this->info(sprintf("匯入 %s-%s-%s %s 時資料", $year, $month, $day, $hour));
-                            $bar->setFormat('debug');
-                            $bar->setRedrawFrequency(300);
-                            $bar->start();
-                            $i = 0;
-                            foreach ($rows as $data) {
-                                $bar->advance();
-                                $data[1] = strtotime($data[1]);
-                                $data[3] = strtotime($data[3]);
-                                $import_data[$i++] = [
-                                    'VehicleType' => $data[0],
-                                    'DerectionTime_O' => $data[1],
-                                    'GantryID_O' => $data[2],
-                                    'DerectionTime_D' => $data[3],
-                                    'GantryID_D' => $data[4],
-                                    'TripLength' => $data[5],
-                                    'TripEnd' => $data[6],
-                                    'TripInformation' => $data[7],
-                                ];
-                                if (strlen(serialize($import_data)) > env('MAX_BUNDLE', 1048576)) {
-                                    DB::disableQueryLog();
-                                    DB::table('trips')->insert($import_data);
-                                    $import_data = [];
-                                    $i = 0;
-                                }
-                            }
-                            if ($i > 0) {
-                                DB::disableQueryLog();
-                                DB::table('trips')->insert($import_data);
-                            }
-                            $bar->finish();
+                            $this->freeway->import(
+                                $year,
+                                $month,
+                                $day,
+                                $hour,
+                                $this->output
+                            );
                         } catch (\Exception $e) {
                             $this->error(substr($e->getMessage(), 0, 100));
                             $this->error(sprintf("%s-%s-%s %s檔案不存在，不執行匯入工作", $year, $month, $day, $hour));
@@ -112,33 +77,5 @@ class import extends Command
                 }
             }
         }
-    }
-
-    private function getLineCount($date, $hour)
-    {
-        $file  = sprintf($this->path . '/%s/%s/TDCS_M06A_%1$s_%2$s0000.csv', $date, $hour);
-        $linecount = 0;
-        $handle = fopen($file, "r");
-        while(!feof($handle)){
-            $line = fgets($handle);
-            $linecount++;
-        }
-
-        fclose($handle);
-
-        return $linecount;
-    }
-
-    private function getRow($date, $hour)
-    {
-        $file  = sprintf($this->path . '/%s/%s/TDCS_M06A_%1$s_%2$s0000.csv', $date, $hour);
-        $fp = fopen($file, 'r');
-        if (false === $fp) {
-            return false;
-        }
-        while ($row = fgetcsv($fp)) {
-            yield $row;
-        }
-        fclose($fp);
     }
 }
