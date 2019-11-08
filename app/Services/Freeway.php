11e66f9eea2291a5bf7dfa2886bb58a990d6a7f7
bency <bency@pixnet.tw>
2019-11-09 02:27:29 +0800

    搬移 downloadFile 功能到 Freeway

--- app/Services/Freeway.php
+++ app/Services/Freeway.php
@@ -12,6 +12,61 @@ class Freeway
         $this->path = $path;
     }
 
+    public function downloadFile($date = '20150101', $output = null) {
+        // Initialize a file URL to the variable
+        $url = 'http://tisvcloud.freeway.gov.tw/history/TDCS/M06A/M06A_' . $date .'.tar.gz';
+
+        // Use basename() function to return the base name of file
+        $file_name = $this->path . '/' . basename($url);
+
+        // Use file_get_contents() function to get the file
+        // from url and use file_put_contents() function to
+        // save the file by using base name
+        // if(file_put_contents($file_name, file_get_contents($url))) {
+        //     echo "File {$date}.tar.gz downloaded successfully";
+        // }
+        // else {
+        //     echo "File {$date}.tar.gz downloading failed.";
+        // }
+        //$this->info("\nDownloading {$date}");
+        $this->bar = $output->createProgressBar(100);
+        // $this->bar->setFormat('debug');
+        // $this->bar->setRedrawFrequency(300);
+        $this->bar->start();
+        //save progress to variable instead of a file
+        $temp_progress = '';
+        $targetFile = fopen($file_name, 'w');
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_NOPROGRESS, false);
+        curl_setopt($ch, CURLOPT_PROGRESSFUNCTION, [$this, 'progressCallback']);
+        curl_setopt($ch, CURLOPT_FILE, $targetFile);
+        curl_exec($ch);
+        fclose($targetFile);
+        //must add $resource to the function after a newer php version. Previous comments states php 5.5
+        system('tar -xzf ' . $file_name);
+        $this->bar->finish();
+    }
+
+    private function progressCallback( $resource, $download_size, $downloaded_size, $upload_size, $uploaded_size )
+    {
+        static $previousProgress = 0;
+
+        if ( $download_size == 0 ) {
+            $progress = 0;
+        } else {
+            $progress = round( $downloaded_size * 100 / $download_size );
+        }
+
+        if ( $progress > $previousProgress)
+        {
+            $previousProgress = $progress;
+            $temp_progress = $progress;
+        }
+
+        $this->bar->setProgress($progress);
+    }
+
     public function import($date, $output)
     {
         $year = substr($date, 0, 4);
