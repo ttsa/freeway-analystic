83f8ceab72140ac1e393eaab6c6c935514a48670
bency <bency@pixnet.tw>
2019-11-08 20:59:45 +0800

    init import_data

--- app/Console/Commands/import.php
+++ app/Console/Commands/import.php
@@ -44,6 +44,7 @@ class import extends Command
         $start_year = $this->ask("要從哪一年開始匯入?", 2015);
         $start_month = $this->ask("要從哪一月開始匯入?", 1);
         $start_day = $this->ask("要從哪一天開始匯入?", 1);
+        $import_data = [];
         for ($year = $start_year; $year <= 2019; $year++) {
             for ($month = 1; $month <= 12; $month++) {
                 if ($year == $start_year and $month < $start_month) {
